38a7976597f6f13df2e56decd7550fe511327fc8
Shamitha Ranasinghe <shamitha4@hotmail.com>
2015-10-30 21:47:04 +0000

    Initial OpenCL testing

---
+++ ENGINE_x64/EngineKernel.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <CL/cl.h>
+#include <CL/cl_platform.h>
+#include <CL/opencl.h>
+
+int main()
+{
+	//Platform(driver) config
+	cl_uint platformIdCount;
+	clGetPlatformIDs(0, nullptr, &platformIdCount);
+
+	std::vector<cl_platform_id> platformIds(platformIdCount);
+	clGetPlatformIDs(platformIdCount, platformIds.data(), nullptr);
+	
+
+	int i;
+	std::cin >> i;
+}
