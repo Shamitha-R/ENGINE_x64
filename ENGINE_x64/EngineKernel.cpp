684d22e189b822f7989a7c96102eba4b2a27e47c
Shamitha Ranasinghe <shamitha4@hotmail.com>
2015-11-01 19:43:50 +0000

    Loading OCT data

--- ENGINE_x64/EngineKernel.cpp
+++ ENGINE_x64/EngineKernel.cpp
@@ -3,16 +3,41 @@
 #include <CL/cl.h>
 #include <CL/cl_platform.h>
 #include <CL/opencl.h>
+#include <fstream>
+#include <string>
 
+void LoadFileData(std::string fileName,std::vector<std::string> &data)
+{
+	std::string currentLine;
+	std::ifstream fileStreamer(fileName);
+
+	if(fileStreamer.is_open())
+	{
+		while (getline(fileStreamer, currentLine))
+			data.push_back(currentLine);
+
+		fileStreamer.close();
+		std::cout << fileName + " successfully read\n";
+
+	}else std::cout << "Unable to open file";
+}
+
+void LoadOCTData()
+{
+	std::cout << "Loading OCT data\n";
+
+	std::vector<std::string> resamplingTableData;
+	std::vector<std::string> referenceAScanData;
+	std::vector<std::string> referenceSpectrumData;
+
+	LoadFileData("resamplingTable.csv", resamplingTableData);
+	LoadFileData("referenceAScan.csv", referenceAScanData);
+	LoadFileData("referenceSpectrum.csv", referenceSpectrumData);
+}
 
 int main()
 {
-	//Platform(driver) config
-	cl_uint platformIdCount;
-	clGetPlatformIDs(0, nullptr, &platformIdCount);
-
-	std::vector<cl_platform_id> platformIds(platformIdCount);
-	clGetPlatformIDs(platformIdCount, platformIds.data(), nullptr);
+	LoadOCTData();
 	
 	int i;
 	std::cin >> i;
